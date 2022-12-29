be00efaf3f85a6a241eca625e6fc1a1b3ea5e228
Aaron <aphexlog@gmail.com>
2022-12-29 17:40:39 -0600

    removing js shit from code and moving it to gitignore

--- src/index.d.ts
+++
@@ -1,8 +0,0 @@
-import { Construct } from 'constructs';
-export interface MyConstructProps {
-    readonly domainName: string;
-    readonly webAssetPath: string;
-}
-export declare class MyConstruct extends Construct {
-    constructor(scope: Construct, id: string, props: MyConstructProps);
-}
