df0968b6f52a2d47e149918c311468ff3d673744
Aaron <aphexlog@gmail.com>
2023-01-07 14:37:39 -0600

    circleci project setup (#22)
    * Add .circleci/config.yml
    
    * adding ci action
    
    * adding ci action
    
    * replacing apache license w MIT
    
    * replacing MIT license path in readme
    
    * changing format of license file
    
    * adding cci params
    
    * bumping node version
    
    * will output cf template in json – bumping node version

--- src/index.ts
+++ src/index.ts
@@ -1,91 +1,32 @@
+import { App } from 'aws-cdk-lib';
 
-    import { RemovalPolicy, CfnOutput, Duration } from 'aws-cdk-lib';
-    import { Construct } from 'constructs';
+import { StaticSiteStack } from './app';
 
-    import { Bucket, BucketEncryption } from 'aws-cdk-lib/aws-s3';
-    import { RecordTarget, ARecord, HostedZone } from 'aws-cdk-lib/aws-route53';
-    import { DnsValidatedCertificate } from 'aws-cdk-lib/aws-certificatemanager';
-    import { BucketDeployment, Source } from 'aws-cdk-lib/aws-s3-deployment';
-    import { Distribution, AllowedMethods, ViewerProtocolPolicy } from 'aws-cdk-lib/aws-cloudfront';
-    import { S3Origin } from 'aws-cdk-lib/aws-cloudfront-origins';
-    import { PolicyStatement, ServicePrincipal } from 'aws-cdk-lib/aws-iam';
-    import { CloudFrontTarget } from 'aws-cdk-lib/aws-route53-targets';
+interface ApplicationProps {
+    readonly domainName: string;
+    readonly webAssetPath: string;
+    readonly env?: { account: string, region: string };
+}
+class Application {
+    constructor(props: ApplicationProps) {
 
-    // create a module
-    export interface Props {
-        readonly domainName: string;
-        readonly webAssetPath: string;
-    }
-
-    export class Blip extends Construct {
-        constructor(scope: Construct, id: string, props: Props) {
-            super(scope, id);
-
-            const bucket = new Bucket(this, 'Bucket', {
-                removalPolicy: RemovalPolicy.DESTROY,
-                autoDeleteObjects: true,
-                websiteIndexDocument: 'index.html',
-                websiteErrorDocument: 'index.html',
-                versioned: true,
-                encryption: BucketEncryption.S3_MANAGED,
-            });
-
-            const zone = HostedZone.fromLookup(this, 'Zone', {
-                domainName: props.domainName,
-            });
-
-            const certificate = new DnsValidatedCertificate(this, 'Certificate', {
-                domainName: props.domainName,
-                hostedZone: zone,
+        const app = new App();
+        const stack = new StaticSiteStack(app, 'StaticSiteStack', {
+            env: {
+                account: '764114738171',
                 region: 'us-east-1',
-            });
+            },
+            domainName: props.domainName,
+            webAssetPath: props.webAssetPath,
+        });
 
-            new BucketDeployment(this, 'DeployToBucket', {
-                sources: [Source.asset(props.webAssetPath)],
-                destinationBucket: bucket,
-            });
+        const synth = app.synth();
+        const template = synth.getStackByName(stack.stackName).template;
 
-            const distribution = new Distribution(this, 'Distribution', {
-                defaultBehavior: {
-                origin: new S3Origin(bucket),
-                allowedMethods: AllowedMethods.ALLOW_GET_HEAD,
-                viewerProtocolPolicy: ViewerProtocolPolicy.REDIRECT_TO_HTTPS
-                },
-                enabled: true,
-                domainNames: [props.domainName],
-                certificate: certificate,
-                enableLogging: true,
-                logFilePrefix: 'aaronwest.me/distribution-logs',
-                defaultRootObject: 'index.html',
-            });
+        return template;
 
-            bucket.addToResourcePolicy(new PolicyStatement({
-                actions: ['s3:GetObject'],
-                resources: [bucket.arnForObjects('*')],
-                principals: [new ServicePrincipal('cloudfront.amazonaws.com')],
-                conditions: {
-                StringEquals: {'aws:Referer': certificate.certificateArn}
-                },
-            }));
-
-            const record = new ARecord(this, 'AliasRecord', {
-                zone,
-                recordName: props.domainName,
-                target: RecordTarget.fromAlias(new CloudFrontTarget(distribution)),
-                deleteExisting: true,
-                ttl: Duration.seconds(60),
-            });
-
-            new CfnOutput(this, 'DistributionDomainName', {
-                value: distribution.distributionDomainName,
-            });
-            new CfnOutput(this, 'DomainName', {
-                value: record.domainName,
-            });
-            new CfnOutput(this, 'BucketUrl', {
-                value: bucket.bucketWebsiteUrl,
-            });
-
-
-        }
     }
+
+}
+
+export { Application };
