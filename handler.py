05272026e777836d6c963270e4ee8239878208ed
billy <aphexlog@gmail.com>
2024-01-17 12:12:10 -0600

    Adding some serverless backend components

--- handler.py
+++ handler.py
@@ -1,10 +1,35 @@
 import json
+import boto3
+from aws_lambda_powertools import Logger
+from botocore.exceptions import ClientError
 
+logger = Logger(service="my-service", leve="INFO")
+
+S3 = boto3.client("s3")
 
 def hello(event, context):
-    body = {
-        "message": "Go Serverless v3.0! Your function executed successfully!",
-        "input": event,
+    """
+    A lambda function that returns an s3 presigned url.
+    The nature of the url (upload or download) is deturmined by the event body.
+
+    :param event
+    """
+    event = json.loads(event["body"])
+
+    params = {
+        "Bucket": "blahBucket",
+        "key": "blahKey",
     }
 
-    return {"statusCode": 200, "body": json.dumps(body)}
+    if event["action"] == "upload":
+        S3.generate_presigned_url(
+            ClientMethod="put_opject",
+            Params=params,
+        )
+    elif event["action"] == "download":
+        S3.generate_presigned_url(
+            ClientMethod="get_object",
+            Params=params,
+        )
+    else:
+        raise ClientError("Invalid action", "invalid action")
